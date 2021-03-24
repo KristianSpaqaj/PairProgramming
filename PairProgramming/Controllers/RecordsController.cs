84a04bc7599e3f250eac95a40def5c900f327433
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-03-24 09:55:25 +0100

    Added Record, Controller and Manager

---
+++ PairProgramming/Controllers/RecordsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PairProgramming.Managers;
+using PairProgramming.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PairProgramming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        private static RecordManager _manager = new RecordManager(new List<Record>()
+        {
+
+        });
+
+
+        // GET: api/<RecordsController>
+        [HttpGet]
+        public IEnumerable<Record> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<RecordsController>/5
+        [HttpGet("{id}")]
+        public Record Get(int id)
+        {
+            return _manager.GetById();
+        }
+
+        // POST api/<RecordsController>
+        [HttpPost]
+        public Record Post([FromBody] Record value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<RecordsController>/5
+        [HttpPut("{id}")]
+        public Record Put(int id, [FromBody] Record value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<RecordsController>/5
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ PairProgramming/Managers/RecordManager.cs
@@ -0,0 +1,41 @@
+﻿using PairProgramming.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PairProgramming.Managers
+{
+    internal class RecordManager
+    {
+        private List<Record> lists;
+
+        public RecordManager(List<Record> lists)
+        {
+            this.lists = lists;
+        }
+
+        internal IEnumerable<Record> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Record GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Record Add(Record value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Record Update(int id, Record value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
