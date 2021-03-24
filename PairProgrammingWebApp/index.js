90e1dbab6b71ac1ec180045c896b22b71e8ff598
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-03-24 13:51:25 +0100

    Added  title search to html

--- PairProgrammingWebApp/index.js
+++ PairProgrammingWebApp/index.js
@@ -1,12 +1,16 @@
 Vue.createApp({
     data(){
         return {
-            records: []
+            records: [],
+            title: "",
+            artist: "",
+            duration: null,
+            releasedate: null
         }
     },
     methods: {
         GetRecords(){
-            axios.get("https://pairprogramming20210324110603.azurewebsites.net/api/records").then(function(response){
+            axios.get("https://pairprogramming20210324110603.azurewebsites.net/api/records?" + "title=" + this.title ).then(function(response){
                 this.records = response["data"];
 
             }.bind(this))
