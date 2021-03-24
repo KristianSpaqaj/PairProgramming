3e09f18cfc4a11a43c7a7d92cac88590663ef86c
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-03-24 09:45:03 +0100

    Add project files.

---
+++ PairProgramming/Model/Record.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PairProgramming.Model
+{
+    public class Record
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public Record()
+        {
+
+        }
+
+        public Record(string title, string artist, int duration, DateTime releaseDate)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            ReleaseDate = releaseDate;
+        }
+    }
+}
