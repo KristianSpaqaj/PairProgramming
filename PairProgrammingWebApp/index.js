820754e7b80346d3d3bdcf62f236c1d5e5e796aa
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-04-08 10:13:15 +0200

    Fiksede Post metode i JS

--- PairProgrammingWebApp/index.js
+++ PairProgrammingWebApp/index.js
@@ -1,34 +1,35 @@
+baseUrl = "https://pairprogramminwebapp.azurewebsites.net/api/records"
 Vue.createApp({
-    data(){
+    data() {
         return {
             records: [],
             title: "",
             artist: "",
-            addData: {title: "", artist: "", duration: null},
+            addData: { title: "", artist: "", duration: null, releaseDate: null },
             duration: null,
             releasedate: null,
             addmessage: ""
         }
     },
     methods: {
-        GetRecords(){
-            axios.get("https://pairprogramming20210324110603.azurewebsites.net/api/records?" + "title=" + this.title ).then(function(response){
+        GetRecords() {
+            axios.get(baseUrl + "?title=" + this.title).then(function (response) {
                 this.records = response["data"];
 
             }.bind(this))
         },
-        async AddRecord()
-        { try {
-            console.log(this.addData)
-            response = await axios.post("https://pairprogramming20210324110603.azurewebsites.net/api/records", this.addData)
-            this.addmessage = "response " + response.status + " " + response.statusText
-        } catch (ex) { 
-            alert(ex.message)
-        } 
+        async AddRecord() {
+            try {
+                console.log(this.addData)
+                response = await axios.post(baseUrl, this.addData)
+                this.addmessage = "response " + response.status + " " + response.statusText
+            } catch (ex) {
+                alert(JSON.stringify(this.addData))
+            }
         }
     },
-        
-    created: function(){
+
+    created: function () {
         this.GetRecords();
     }
 
